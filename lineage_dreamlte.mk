74911bee48edccd2446e93a266b5af64557d34c6
Jan Altensen <info@stricted.net>
2019-08-28 17:51:50 +0200

    oops
    Change-Id: I1097c9237dd23c01864ca47bdf7ff4e6433fb5d9

--- lineage_dreamlte.mk
+++ lineage_dreamlte.mk
@@ -20,6 +20,7 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base.mk)
 
 # Inherit from dreamlte device
 $(call inherit-product, device/samsung/dreamlte/device.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
 # Inherit some common Lineage stuff.
 $(call inherit-product, vendor/lineage/config/common_full_phone.mk)
